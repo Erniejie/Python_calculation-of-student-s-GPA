b860efc018426987fe07d4fceedff56a4c8904d6
erniejie <46618858+Erniejie@users.noreply.github.com>
2021-04-26 19:11:47 +0000

    Add files via upload

---
+++ 2021-04-26_Python_ Calculates the student's GPA.py
@@ -0,0 +1,36 @@
+#Python_calculation the student's GPA
+#ComputerProgramming Tutor_April 25 2021
+
+classno=None
+
+while classno==None:
+    try:
+        classno=int(input("How Many Subjects are you taking?: "))
+    except:
+        print("Error,Please Enter Numbers!!\n")
+
+output=0
+i=0
+while i<classno:
+    grade=input("Enter your grade in letter for the %s class." %(str(i+1)))
+    grade=grade.upper()
+    if grade== "A" or grade=="B" or grade =="C" or grade=="D"or grade=="F":
+        i+=1
+        if grade=="A":
+            output +=4
+        elif grade=="B":
+            output +=3
+        elif grade =="C":
+            output +=2
+        elif grade=="D":
+            output +=1
+        elif grade=="F":
+            output +=0
+        print(output)
+    else:
+        print("Invalid Grade,Please Enter: A,B,C,D,or F!!\n")
+
+average="%.2f"%(float(output)/float(classno))
+print("Your GPA is:%s" %(str(average)))
+                
+            
